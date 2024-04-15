abdede0214f76bf9e875253719604c0c400f4d9a
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-15 16:24:26 +0500

    edited sln file

--- core/Models/CommonConfigSettings.cs
+++ core/Models/CommonConfigSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleConfigReader.Core.Models;
+﻿namespace SimpleConfigReader.Core.Models;
 
 /// <summary>
 /// Common config settings.
