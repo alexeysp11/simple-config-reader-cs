ff4b56476b44fa96dba2f0714b1ea59ed24ac26c
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-15 13:08:04 +0500

    core: added CommonConfigSettings model

---
+++ core/Models/CommonConfigSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConfigReader.Core.Models;
+
+/// <summary>
+/// Common config settings.
+/// </summary>
+public class CommonConfigSettings
+{
+    /// <summary>
+    /// Directory path.
+    /// </summary>
+    public string? DirectoryPath { get; set; }
+}
